6aeeffeb59a380a27e7a134b876a28b6a01de32f
Aron <thummper@hotmail.co.uk>
2018-07-06 14:39:56 +0100

    Initial upload.
    In so far: 
    
    - Drag, drop and rotate shapes on board. 
    - Areas for the shapes to spawn (buggy)

---
+++ carp.js
@@ -0,0 +1,221 @@
+window.onload = function () {
+	document.body.style.opacity = "1";
+
+	//Start script
+	setup();
+};
+//variables 
+let tileSize = 34;
+let width = 22;
+let height = width;
+let xspan;
+let yspan;
+let mx;
+let my;
+let canvas;
+let ctx;
+let lc = 20;
+//Used for drawing shape x/y
+let testx;
+let testy;
+let shapes = [];
+let draggingShapes = [];
+let pieces = [
+	[I, "cyan"],
+	[J, "blue"],
+	[L, "orange"],
+	[O, "yellow"],
+	[S, "green"],
+	[T, "purple"],
+	[Z, "red"]
+];
+let board = [];
+let holder;
+
+
+
+
+
+function setup() {
+	canvas = document.getElementById("carp_canvas");
+	ctx = canvas.getContext("2d");
+	canvas.width = tileSize * width;
+	canvas.height = tileSize * height;
+
+	//Set up the gameboard. 
+	for (let row = 0; row < height; row++) {
+		board[row] = [];
+		for (let col = 0; col < width; col++) {
+			board[row][col] = {
+				x: col * tileSize,
+				y: row * tileSize,
+				contains: false
+			};
+		}
+	}
+	holder = new Holder();
+	holder.makeSpaces();
+
+	//Add event listeners.
+	add_event_listeners(canvas);
+	//Start the game
+	loop();
+}
+
+function loop() {
+	//Clear canvas
+	clear();
+	//Draw the grid
+	resetBoard();
+	//Add all shapes to the grid.
+	for (i in shapes) {
+		let shape = shapes[i];
+		shape.draw();
+	}
+	//Draw grid and shapes.
+	
+	drawBoard();
+	holder.drawSpace();
+	holder.trySpawn();
+	
+	
+	
+	//Draw the dragging shape on the mouse.
+	for (i in draggingShapes) {
+		let shape = draggingShapes[i];
+		shape.drag();
+		shape.draw_on_mouse();
+	}
+	
+	//Draw the x/y of the dragging shape.
+	if (testx && testy) {
+		rect(testx, testy, tileSize, tileSize, "yellow");
+	}
+	
+	//Run loop as fast as possible.
+	window.requestAnimationFrame(loop);
+}
+//Functions for gameloop
+function drawBoard() {
+	for (let row = 0; row < width; row++) {
+		for (let col = 0; col < height; col++) {
+			if (board[row][col].contains) {
+				rect(col * tileSize, row * tileSize, tileSize, tileSize, board[row][col].contains.colour, "rgba(0,0,0,0.3)");
+
+			} else {
+				rect(col * tileSize, row * tileSize, tileSize, tileSize, "white", "rgba(0,0,0,0.3)");
+
+			}
+		}
+	}
+}
+
+function resetBoard() {
+	//reset the game board for next loop. 
+	for (let row = 0; row < height; row++) {
+		board[row] = [];
+		for (let col = 0; col < width; col++) {
+			board[row][col] = {
+				x: col * tileSize,
+				y: row * tileSize,
+				contains: false
+			};
+		}
+	}
+}
+
+function rect(x, y, w, h, colour, border) {
+	//Draw a rectangle with various inputs.
+	if (arguments.length == 6) {
+		ctx.beginPath();
+		ctx.rect(x, y, w, h);
+		ctx.fillStyle = colour;
+		ctx.strokeStyle = border;
+		ctx.fill();
+		ctx.stroke();
+		ctx.closePath();
+
+	} else if (arguments.length == 4) {
+		//Draw black rect 
+		ctx.beginPath();
+		ctx.rect(x, y, w, h);
+		ctx.fill();
+		ctx.closePath();
+
+	} else {
+		//Draw rect with colour. 
+		ctx.beginPath();
+		ctx.fillStyle = colour;
+		ctx.rect(x, y, w, h);
+		ctx.fill();
+		ctx.closePath();
+
+	}
+
+}
+//MOVE TO SHAPE.
+function drag(shape) {
+	let blocknumber = -1;
+	let bc = 0;
+	let br = 0;
+
+	for (let i = 0; i < shape.pattern.length; i++) {
+		for (let j = 0; j < shape.pattern[i].length; j++) {
+			if (shape.pattern[i][j]) {
+				//Non 0 value. 
+				blocknumber += 1;
+				if (blocknumber == shape.blockdrag) {
+					//Found the block we are dragging. 
+					bc = i;
+					br = j;
+				}
+			}
+		}
+	}
+	//TODO figure out what this does.
+	shape.x = mx - (bc * xspan) - xspan / 2;
+	shape.y = my - (br * yspan) - yspan / 2;
+	//shape.x = mx;
+	//shape.y = my;
+
+
+
+}
+//MOVE TO SHAPE
+function gridSnap(shape) {
+	//Snap the shape to the nearest gridpoint.
+	let xrem = mx % tileSize;
+	let yrem = my % tileSize;
+	let xsnap, ysnap;
+	xsnap = mx - xrem;
+	ysnap = my - yrem;
+	//Find the block we are dragging and translate the coords.
+	xsnap -= Math.floor((shape.pattern[0].length)/2) * tileSize;
+	ysnap -= Math.floor((shape.pattern.length)/2   ) * tileSize;
+	//Don't let shape off the canvas.
+	if(xsnap < 0){
+		xsnap = 0;
+	}
+	if(ysnap < 0){
+		ysnap = 0;
+	}
+	testx = xsnap;
+	testy = ysnap;
+	shape.x = xsnap;
+	shape.y = ysnap;
+}
+
+function random_range(min, max) {
+	//Return random positive integer. 
+	return Math.round(Math.random() * (max - min) + 1);
+}
+
+function clear() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+let cols = ["red", "orange", "green", "purple", "yellow", "teal", "cyan", "magenta", "darkorange", "black", "grey"];
+function randomcol(){
+	return cols[random_range(0, cols.length)];
+	
+}
---
+++ holder.js
@@ -0,0 +1,104 @@
+var Holder = function () {
+	//Max pattern size is 5 x 5, the bottom 5 grid spaces are reserved for spawning pieces.
+	//When holder is made, the board is already defined.
+	this.padding = 1;
+	this.padSpaces;
+	this.y = canvas.height - ((5 + this.padding) * tileSize);
+	this.hasPiece = true;
+	this.width = this.height = 5; //5 tiles.
+	//Hold all spaces in an array (like shape blocks)
+	this.spaces = [];
+
+}
+Holder.prototype.checkSpaces = function(){
+	console.log("check spaces");
+	//Checks each space to see if it is empty. 
+	for(let k in this.spaces){
+		
+		let space = this.spaces[k];
+		//Do a similar thing to the shape draw function - check if the grid is empty based on x,y,w,h.
+		console.log("SPACE %i/%i", space.x, space.y);
+		
+		
+		for(let row in board){
+			
+			for(let col in board[row]){
+				
+				let cell = board[row][col];
+				
+				if(cell.x == space.x && cell.y == space.y){
+					console.log("CELL MATCH: %i/%i", cell.x, cell.y);
+					//Found the cell 
+					let free = true;
+					
+					for(let i = 0; i < this.height; i++){
+						for(let j = 0; j < this.width; j++){
+							if(board[parseInt(row) + i][parseInt(col) + j].contains){
+								free = false;
+							}
+							
+							
+
+						}
+					}
+					if(free){
+						space.piece = false;
+						console.log("Space %i is free", parseInt(k));
+					} else {
+						console.log("Space %i is not free", parseInt(k));	
+					}
+				}
+			}
+		}
+		
+	}
+	
+}
+Holder.prototype.trySpawn = function(){
+	//Will attempt to spawn a new shape. 
+	for(let i in this.spaces){
+		let space = this.spaces[i];
+		if(space.piece == false){
+			//Space is empty, we can spawn a shape in it. 
+			space.piece = true;
+			shapes.push(new Shape(randomcol(), space.x, space.y));
+		}
+	}
+	
+}
+Holder.prototype.makeSpaces = function(){
+	//Set up the shape spaces.
+	let spaces = Math.floor((width - this.padding) / this.width);
+	this.padSpaces = (width - this.padding) % 5;
+	console.log("SPACES AVAILAIBLE: " + spaces + " PADDING: " + this.padSpaces );
+	//Ignore padding for now as width/height is static.
+	
+	for(let i = 0; i < spaces; i++){
+		let extra = 0;
+		if(this.padSpaces > 0){
+			extra = 1
+			this.padSpaces--;
+			
+		}
+		this.spaces.push({
+			
+			x: 			(this.padding * tileSize) + (i * (5 + extra) * tileSize),
+			y: 			this.y,
+			w: 			this.width,
+			h:			this.height,
+			piece: 		false,
+			bg:			"rgba(255, 165, 0, 0.2)"
+			
+		});
+	}
+};
+
+Holder.prototype.drawSpace = function(){
+	//Loops through spaces array and draws on the board. 
+	for(let i in this.spaces){
+		let space = this.spaces[i];
+		//Draw
+		rect(space.x, space.y, space.w * tileSize, space.h * tileSize, space.bg, "black");
+	}
+}
+
